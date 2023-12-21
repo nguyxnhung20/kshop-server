7044041db76892d9fe5898ef265e347ef5b49e09
nguyxnhung20 <nguyenduyhung.20052002@gmail.com>
2023-12-21 21:48:14 +0700

    [Add] Car Shop Project

---
+++ src/test/java/com/vti/kshop/KshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.kshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
